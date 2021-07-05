c49bb1f4aef3158a69abcf9eb7a78e74320bba82
Irfan Ahmed <irfanahmed0915@gmail.com>
2021-07-04 23:23:05 -0500

    Another round of feedback

--- frontend/src/components/commands.jsx
+++ frontend/src/components/commands.jsx
@@ -20,6 +20,7 @@ class Command extends Component {
       index: 0,
       currentText: "",
       endOfCommands: false,
+      progress: 0,
     };
   }
 
@@ -41,42 +42,80 @@ class Command extends Component {
 
   incrementCommand() {
     var { index, text } = this.state;
+    var progress = Math.floor(((index + 1) / text.length) * 100);
     index = index + 1;
-    if (index === text.length) {
+    if (index >= text.length) {
       this.setState({
+        index: text.length,
         endOfCommands: true,
+        progress: 100,
       });
     } else {
       this.setState({
         index: index,
         currentText: text[index],
+        progress: progress,
       });
     }
   }
 
   render() {
-    const { currentText, endOfCommands } = this.state;
+    const { currentText, endOfCommands, progress } = this.state;
 
     let displayText;
 
     if (endOfCommands) {
       displayText = (
-        <div className="text-white m-2 rounded text-center bg-success">
-          Thank You for recording data!!
+        <div
+          className="text-white m-2 rounded text-center bg-success"
+          style={{ height: "100px" }}
+        >
+          <h1>Thank You for recording data!</h1>
         </div>
       );
     } else {
       displayText = (
         <div
-          className="text-white m-2 rounded text-center bg-dark"
+          className="text-black m-2 rounded"
+          style={{ height: "100px" }}
           key={currentText}
         >
-          {currentText}
+          <h1 className="text-center">{currentText}</h1>
         </div>
       );
     }
 
-    return displayText;
+    return (
+      <div>
+        <div className="row m-2">
+          <p>
+            <strong>
+              Progress : {this.state.index}/{this.state.text.length}
+            </strong>
+          </p>
+          <div className="progress" style={{ height: "20px" }}>
+            <div
+              className="progress-bar progress-bar-striped progress-bar-animated"
+              role="progressbar"
+              style={{ width: this.state.progress + "%", color: "black" }}
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+        <div className="bg-light">
+          <p className="text-info">
+            <i>
+              <strong>
+                <u>Please say command as you normally do</u>
+              </strong>
+            </i>
+          </p>
+          {displayText}
+        </div>
+      </div>
+    );
   }
 }
 
